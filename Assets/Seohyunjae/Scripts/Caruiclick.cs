773bd0213308d577aa4ab8bdcfa988e1b6830bd9
seohyunjae <97527644+seohyunjae@users.noreply.github.com>
2023-10-09 15:53:31 +0900

    수정

--- Assets/Seohyunjae/Scripts/Caruiclick.cs
+++ Assets/Seohyunjae/Scripts/Caruiclick.cs
@@ -18,8 +18,7 @@ public class Caruiclick : MonoBehaviour
     public static bool carin_bool;
 
     Carmovement carmovement;
-    //public GameObject camera;
-    // Start is called before the first frame update
+
 
     private void Awake()
     {
@@ -29,26 +28,17 @@ public class Caruiclick : MonoBehaviour
 
     public void yesOnClick()
     {
-        //Debug.Log(123);
+       
         carin_bool = true;
-        //carcamera.SetActive(true);
-       // Debug.Log("차량ui클릭");
-       // Debug.Log(car.transform.position);
+ 
         carui.SetActive(false);
-        //player.SetActive(false);
+       
 
-        player.transform.parent = carin_tranform.transform; //자식으로 붙히고 탑승시작
+        player.transform.parent = carin_tranform.transform; 
         player.transform.position = carin_tranform.transform.position;
         player.transform.rotation = carin_tranform.transform.rotation;
 
-
-
-
-
-        //플레이어탑승함
-
-        //플레이어 이동하고 텔포금지
-        player.GetComponent<TeleportationProvider>().enabled = false; //텔포금지
+        player.GetComponent<TeleportationProvider>().enabled = false; 
         player.GetComponent<DynamicMoveProvider>().enabled = false;
         player.GetComponentsInChildren<GrabMoveProvider>()[0].enabled = false;
         player.GetComponentsInChildren<GrabMoveProvider>()[1].enabled = false;
@@ -59,7 +49,7 @@ public class Caruiclick : MonoBehaviour
 
 
 
-        //player.transform.position = car.transform.position + new Vector3(4f, 0, 0);
+ 
     }
     public void noOnClick()
     {
@@ -68,11 +58,7 @@ public class Caruiclick : MonoBehaviour
         Carmovement carmovement = this.GetComponentInParent<Carmovement>();
         carmovement.isNoclick = true;
         carui.SetActive(false);
-        //carmovement.inplayer = false;
-        //player.transform.position = car.transform.position + new Vector3(-2f, 2f, 0);
-        //player.transform.parent = null;
-        //player.SetActive(true);
-
+     
     }
 }
 
